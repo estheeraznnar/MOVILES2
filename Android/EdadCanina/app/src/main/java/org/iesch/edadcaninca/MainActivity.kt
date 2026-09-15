2395a9dfc524aa6f65f2d75de9b15ee58db803fe
estheeraznnar <estheeraznnar2@gmail.com>
2026-09-15 21:16:02 +0200

    edad canina

---
+++ Android/EdadCanina/app/src/main/java/org/iesch/edadcaninca/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.iesch.edadcaninca
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //Aqui la pantalla esta creada:
+
+        //1- Tomamos el control d etodos los elementos del UI
+    }
+}
