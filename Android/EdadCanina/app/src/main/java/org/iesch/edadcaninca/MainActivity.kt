f0ef111eed97a3ad6e72af48b168b3124f368d64
estheeraznnar <estheeraznnar2@gmail.com>
2026-09-17 18:22:58 +0200

    fin edad canina e inicio superheroes

--- Android/EdadCanina/app/src/main/java/org/iesch/edadcaninca/MainActivity.kt
+++ Android/EdadCanina/app/src/main/java/org/iesch/edadcaninca/MainActivity.kt
@@ -1,6 +1,10 @@
 package org.iesch.edadcaninca
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,5 +23,30 @@ class MainActivity : AppCompatActivity() {
         //Aqui la pantalla esta creada:
 
         //1- Tomamos el control d etodos los elementos del UI
+        val resultText = findViewById<TextView>(R.id.texto_respuesta)
+        val calcularBoton = findViewById<Button>(R.id.boton)
+        val editText = findViewById<EditText>(R.id.edit)
+
+
+
+        //2- Los botones tienen la propiedad setOnClickListener para que sean clicables
+        calcularBoton.setOnClickListener {
+            //aqui metemos el codigo d elo que queremos hacer cuando pulsemos el boton
+            val edadPerro = editText.text.toString()
+            if (edadPerro.isEmpty()){
+                //3- Mostramos un mensaje de tipo toas
+                //resultText.text = "Introduce una edad"
+                val mensaje = Toast.makeText(this, "Debes de introducir una edad", Toast.LENGTH_LONG)
+                mensaje.show()
+            }else{
+                //tenemos que pasar el valor a entero
+                val edadInt = edadPerro.toInt()
+                val dogAge = edadInt * 7
+
+                resultText.text = "Si fueras un perro tu edad seria de ${dogAge} años"
+            }
+
+        }
+
     }
 }
